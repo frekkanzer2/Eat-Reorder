af09da693ea8778839bf0e68fac05b7ee671b7f6
Marco Dello Buono <49031319+MarcoProduction@users.noreply.github.com>
2020-01-02 20:27:33 +0100

    Creazione Classe GestoreUtenteDaoImpl (Da completare)
    E' stata creata la classe GestoreUtenteDaoImpl con alcuni metodi (Da completare)

--- database_source/CreazioneTabelleDB.sql
+++ database_source/CreazioneTabelleDB.sql
@@ -120,41 +120,41 @@ primary key(prodotto,ordine)
 #creaOrdine(order : Ordine, user : AccountUtente, cart : Carrello)
 select Fattorino.email, Fattorino.nome, GiorniLavorativi.giorno, Fattorino.orario_inizio, Fattorino.orario_fine
 from Fattorino, GiorniLavorativi
-where Fattorino.citta_consegna='?' and GiorniLavorativi.giorno='?' and Fattorino.orario_inizio<'?' and Fattorino.orario_fine>'?';
+where Fattorino.citta_consegna=? and GiorniLavorativi.giorno=? and Fattorino.orario_inizio<? and Fattorino.orario_fine>?;
 
 insert into Ordine (indirizzo_consegna, numero_carta, prezzo_totale, note, stato, acquirente, email_acquirente, azienda, email_azienda, fattorino, email_fattorino) values 
-("?","?",?,"?","?","?","?","?","?","?","?");
+(?,?,?,?,?,?,?,?,?,?,?);
 
 insert into ProdottoOrdine (quantita, prodotto, ordine) values
-(?, "?", ?);
+(?, ?, ?);
 
 #controlloEsistenzaOrdine(orderCode: Long)
 select *
 from Ordine 
-where Ordine.codice='?';  #codice dell'ordine
+where Ordine.codice=?;  #codice dell'ordine
 
 #dammiOrdiniPreparazione(azienda : AccountAzienda)
 select Ordine.codice, Ordine.stato
 from Ordine
-where Ordine.stato='?' and Ordine.email_azienda='?';  #preparazione
+where Ordine.stato=? and Ordine.email_azienda=?;  #preparazione
 
 #dammiOrdine(codice : Long)
 select *
 from Ordine
-where Ordine.codice='?';
+where Ordine.codice=?;
 
 #dammiConsegne(fattorino : AccountFattorino)
 select Ordine.codice, Fattorino.email, Fattorino.nome
 from Fattorino, Ordine
-where Fattorino.email='?';
+where Fattorino.email=?;
 
 #ordineSetRitirato(orderCode : Long)
 update Ordine
-set Ordine.stato='?';  #ritirato
+set Ordine.stato=?;  #ritirato
 
 #ordineSetConsegnato(orderCode : Long)
 update Ordine
-set Ordine.stato='?';  #consegnato
+set Ordine.stato=?;  #consegnato
 
 
 
@@ -163,7 +163,7 @@ set Ordine.stato='?';  #consegnato
 #controlloEsistenzaMail(email: String)
 select * 
 from UtenteRegistrato
-where UtenteRegistrato.email='?';
+where UtenteRegistrato.email=?;
 
 #registrazioneCliente(cliente: AccountCliente)
 create table Cliente(
@@ -174,6 +174,19 @@ on update cascade
 on delete cascade,
 primary key(email, nome)
 );
+
+insert into Cliente(nome, cognome, email) values (?,?,?);
+
+insert into Cliente(nome, cognome, email) values ("b","c","a");
+insert into UtenteRegistrato(email, password, tipologia, is_banned) values ("a","a","aaa", true);
+
+update Cliente 
+set Cliente.nome="ddd", Cliente.cognome="ddd", UtenteRegistrato.password="ddd"
+from Cliente, UtenteRegistrato
+where Cliente.email=UtenteRegistrato.email;
+
+select * 
+from UtenteRegistrato;
 
 #registrazioneAzienda(azienda: AccountAzienda)
 create table Azienda(
@@ -192,6 +205,8 @@ on delete cascade,
 primary key(email, nome)
 );
 
+insert into Azienda(nome, via, numero_civico, citta, provincia, partita_iva, telefono, orario_apertura, orario_chiusura, email) values (?,?,?,?,?,?,?,?,?,?);
+
 #registrazioneFattorino(fatt: AccountFattorino)
 create table Fattorino(
 nome varchar(20) not null, 
@@ -207,58 +222,60 @@ on delete cascade,
 primary key(email,nome)
 );
 
+insert into Fattorino(nome, cognome, telefono, citta_consegna, orario_inizio, orario_fine, email) values (?,?,?,?,?,?,?);
+
 #controllaBan(email: String)
 select Azienda.email
 from Azienda
-where Azienda.email='?';
+where Azienda.email=?;
 
 #controllaEsistenzaAccount(user: String, pass: String)
 select * 
 from UtenteRegistrato
-where UtenteRegistrato.email='?' and UtenteRegistrato.password='?';
+where UtenteRegistrato.email=? and UtenteRegistrato.password=?;
 
 #dammiUtente(email: String)
 select *
 from UtenteRegistrato
-where UtenteRegistrato.email='?';
+where UtenteRegistrato.email=?;
 
 #aggiornaCliente(cliente: AccountCliente)		#DaRivedere
 update Cliente, UtenteRegistrato 
-set Cliente.nome='?', Cliente.cognome='?', UtenteRegistrato.password='?';
+set Cliente.nome=?, Cliente.cognome=?, UtenteRegistrato.password=?;
 
 #aggiornaAzienda(azienda: AccountAzienda)     	#DaRivedere
 update Azienda, UtenteRegistrato
-set Azienda.nome='?', Azienda.via='?', Azienda.numero_civico=?, Azienda.citta='?', Azienda.provincia='?', Azienda.partita_iva='?', Azienda.telefono='?', Azienda.orario_apertura='?', Azienda.orario_chiusura='?', UtenteRegistrato.password='?'; 
+set Azienda.nome=?, Azienda.via=?, Azienda.numero_civico=?, Azienda.citta=?, Azienda.provincia=?, Azienda.partita_iva=?, Azienda.telefono=?, Azienda.orario_apertura=?, Azienda.orario_chiusura=?, UtenteRegistrato.password=?; 
 
 #aggiornaFattorino(fatt: AccountFattorino)		#DaRivedere
 update Fattorino, UtenteRegistrato
-set Fattorino.nome='?', Fattorino.cognome='?', Fattorino.telefono='?', Fattorino.citta_consegna='?', Fattorino.provincia='?', Fattorino.orario_inizio='?', Fattorino.orario_fine='?', UtenteRegistrato.password='?';
+set Fattorino.nome=?, Fattorino.cognome=?, Fattorino.telefono=?, Fattorino.citta_consegna=?, Fattorino.provincia=?, Fattorino.orario_inizio=?, Fattorino.orario_fine=?, UtenteRegistrato.password=?;
 
 #dammiAziendaConOrdine(ordine: Ordine)
 select distinct Ordine.email_azienda, Ordine.azienda
 from Ordine
-where Ordine.email_azienda='?'; 
+where Ordine.email_azienda=?; 
 
 #banAzienda(azienda: AccountAzienda)
 select Azienda.email
 from Azienda
-where Azienda.email='?';
+where Azienda.email=?;
 
 #dammiListaAziende(citta: String)
 select *
 from Azienda
-where Azienda.citta='?';
+where Azienda.citta=?;
 
 #aggiungiAlListino(azienda: AccountAzienda, prod : Prodotto)
 select Prodotto.codice
 from Prodotto
-where Prodotto.codice='?' and Prodotto.email='?';
+where Prodotto.codice=? and Prodotto.email=?;
 
 #aggiornaProdotto(azienda: AccountAzienda, prod : Prodotto)
 update Prodotto
-set Prodotto.nome='?', Prodotto.descrizione='?', Prodotto.prezzo=?, Prodotto.path_immagine='?'
-where Prodotto.email='?';
+set Prodotto.nome=?, Prodotto.descrizione=?, Prodotto.prezzo=?, Prodotto.path_immagine=?
+where Prodotto.email=?;
 
 #rimuoviProdotto(azienda: AccountAzienda, prod : Prodotto)
 delete from Prodotto
-where Prodotto.codice='?' and Prodotto.email='?';   
+where Prodotto.codice=? and Prodotto.email=?;   
--- website_source/src/model/bean/GestoreUtenteDAOImpl.java
+++ website_source/src/model/bean/GestoreUtenteDAOImpl.java
@@ -1,5 +1,119 @@
 package model.bean;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class GestoreUtenteDAOImpl {
 
+private Connection connect;
+	
+	public boolean controlloEsistenzaMail(String email) throws SQLException {
+		
+		connect= DBConnectionPool.getConnection();
+		
+		PreparedStatement stmt= connect.prepareStatement("select * from UtenteRegistrato where UtenteRegistrato.email=?");
+		stmt.setString(1, email);
+		
+		ResultSet x= stmt.executeQuery();
+		
+		if(x.next()) {
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	
+	public void registrazioneCliente(AccountCliente_Bean cliente) throws SQLException {
+		
+		connect= DBConnectionPool.getConnection();
+		
+		PreparedStatement stmt= connect.prepareStatement("insert into Cliente(nome, cognome, email) values (?,?,?)");
+		
+		stmt.executeUpdate();
+		
+		return ;
+	}
+	
+	
+	public void registrazioneAzienda(AccountAzienda_Bean azienda) throws SQLException {
+		
+		connect= DBConnectionPool.getConnection();
+		
+		PreparedStatement stmt= connect.prepareStatement("insert into Azienda(nome, via, numero_civico, citta, provincia, partita_iva, telefono, orario_apertura, orario_chiusura, email) values (?,?,?,?,?,?,?,?,?,?)");
+		
+		stmt.executeUpdate();
+		
+		return ;
+	}
+	
+	
+	public void registrazioneFattorino(AccountFattorino_Bean fattorino) throws SQLException {
+		
+		connect= DBConnectionPool.getConnection();
+		
+		PreparedStatement stmt= connect.prepareStatement("insert into Fattorino(nome, cognome, telefono, citta_consegna, orario_inizio, orario_fine, email) values (?,?,?,?,?,?,?)");
+		
+		stmt.executeUpdate();
+		
+		return ;
+	}
+	
+	public boolean controllaBan(String email) throws SQLException {
+		
+		connect= DBConnectionPool.getConnection();
+		
+		PreparedStatement stmt= connect.prepareStatement("select Azienda.email from Azienda where Azienda.email=?");
+		stmt.setString(1, email);
+		
+		ResultSet x= stmt.executeQuery();
+		
+		if(x.next()) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	public boolean controllaEsistenzaAccount(String user, String password) throws SQLException {
+		
+		connect= DBConnectionPool.getConnection();
+		
+		PreparedStatement stmt= connect.prepareStatement("select * from UtenteRegistrato where UtenteRegistrato.email=? and UtenteRegistrato.password=?");
+		stmt.setString(1, user);
+		stmt.setString(2, password);
+		
+		ResultSet x= stmt.executeQuery();
+		
+		if(x.next()) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	public AccountUtenteRegistrato_Bean dammiUtente(String email) {
+		
+		return null;
+	}
+	
+	
+	public void aggiornaCliente(AccountCliente_Bean cliente) throws SQLException {
+		
+		connect= DBConnectionPool.getConnection();
+		
+		PreparedStatement stmt= connect.prepareStatement("update Cliente, UtenteRegistrato set Cliente.nome=?, Cliente.cognome=?, UtenteRegistrato.password=?");
+		stmt.setString(1, cliente.getNome());
+		stmt.setString(2, cliente.getCognome());
+		stmt.setString(3, cliente.getPassword());
+		
+		stmt.executeUpdate();
+		
+		return;
+	}
 }

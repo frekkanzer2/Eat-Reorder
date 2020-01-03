38771c57cc81e1431f1c64549999d1ddb4204e76
Vincenzo De Martino <52664512+kenz097@users.noreply.github.com>
2020-01-03 16:58:15 +0100

    Update LogoutServlet.java

--- website_source/src/controller/LogoutServlet.java
+++ website_source/src/controller/LogoutServlet.java
@@ -28,7 +28,7 @@ public class LogoutServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		session.removeAttribute(""); //attributo da inserire
+		session.removeAttribute("utente"); 
 		session.invalidate();
 		response.sendRedirect("Homepage.jsp");
 	}
